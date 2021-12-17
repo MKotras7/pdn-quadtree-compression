57c9c13740984b1f77f427e1b816fc9ce6342ad7
MKotras7 <mkotras7@gmail.com>
2021-12-17 13:44:01 -0600

    Initial commit from offline work

---
+++ QuadtreecompressorEffect/QuadTree.cs
@@ -0,0 +1,144 @@
+﻿using PaintDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadtreecompressorEffect
+{
+    public class QuadTree
+    {
+        private Surface surface;
+        private (int x, int y) position;
+        private int size;
+        private QuadTree parent;
+        private QuadTree[] children;
+
+        private float variance;
+        private ColorBgra color;
+        float sensitivity;
+
+        public QuadTree(QuadTree parent, Surface surface, (int x, int y) position, int size, float sensitivity)
+        {
+            this.parent = null;
+            this.surface = surface;
+            this.position = position;
+            this.size = size;
+            this.sensitivity = sensitivity;
+
+            this.children = null;
+        }
+
+        public void doPerPixel(Action<ColorBgra> action)
+        {
+            for(int x = position.x; x < position.x + size; x++)
+            {
+                for (int y = position.y; y < position.y + size; y++)
+                {
+                    action(surface[x, y]);
+                }
+            }
+        }
+
+        public ColorBgra getAverage()
+        {
+            //Average = sum of all pixels squared, divide that by n, square root that
+            long squareR = 0;
+            long squareG = 0;
+            long squareB = 0;
+            long squareA = 0;
+            doPerPixel((ColorBgra color) =>
+            {
+                squareR += color.R * color.R;
+                squareG += color.G * color.G;
+                squareB += color.B * color.B;
+                squareA += color.A * color.A;
+            });
+            int numPixels = size * size;
+            squareR /= numPixels;
+            squareG /= numPixels;
+            squareB /= numPixels;
+            squareA /= numPixels;
+            squareR = (long) Math.Sqrt(squareR);
+            squareG = (long) Math.Sqrt(squareG);
+            squareB = (long) Math.Sqrt(squareB);
+            squareA = (long) Math.Sqrt(squareA);
+            return ColorBgra.FromBgra((byte) squareB, (byte)squareG, (byte)squareR, (byte)squareA);
+        }
+
+        public float getVariance()
+        {
+            //Variance is the sum of all pixels' difference from mean, that squared, divide by n-1.
+            ColorBgra average = getAverage();
+            float varianceR = 0.0f;
+            float varianceG = 0.0f;
+            float varianceB = 0.0f;
+            float varianceA = 0.0f;
+            doPerPixel((ColorBgra color) =>
+            {
+                varianceR += MathF.Pow(color.R - average.R, 2);
+                varianceG += MathF.Pow(color.G - average.G, 2);
+                varianceB += MathF.Pow(color.B - average.B, 2);
+                varianceA += MathF.Pow(color.A - average.A, 2);
+            });
+            int numPixels = size * size;
+            int degreesOfFreedom = numPixels - 1;
+            varianceR /= degreesOfFreedom;
+            varianceG /= degreesOfFreedom;
+            varianceB /= degreesOfFreedom;
+            varianceA /= degreesOfFreedom;
+
+            return varianceR + varianceG + varianceB + varianceA;
+        }
+
+        public void compress()
+        {
+            color = getAverage();
+            variance = getVariance();
+            //if(false)
+            if(MathF.Sqrt(variance) >= sensitivity && size > 1)
+            {
+                children = new QuadTree[4];
+                (int x, int y) midPoint = (position.x + (size / 2), position.y + (size / 2));
+
+                children[0] = new QuadTree(this, surface, (position.x, position.y), size / 2, sensitivity);
+                children[1] = new QuadTree(this, surface, (position.x, midPoint.y), size / 2, sensitivity);
+                children[2] = new QuadTree(this, surface, (midPoint.x, position.y), size / 2, sensitivity);
+                children[3] = new QuadTree(this, surface, (midPoint.x, midPoint.y), size / 2, sensitivity);
+                children[0].compress();
+                children[1].compress();
+                children[2].compress();
+                children[3].compress();
+            }
+        }
+
+        public void render(Surface destinationSurface)
+        {
+            if(size == 1)
+            {
+                destinationSurface[position.x, position.y] = color;
+            }
+            else
+            {
+                if(children != null)
+                {
+                    children[0].render(destinationSurface);
+                    children[1].render(destinationSurface);
+                    children[2].render(destinationSurface);
+                    children[3].render(destinationSurface);
+                }
+                else
+                {
+                    for (int x = position.x; x < position.x + size; x++)
+                    {
+                        for (int y = position.y; y < position.y + size; y++)
+                        {
+                            destinationSurface[x, y] = color;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ QuadtreecompressorEffect/Quadtreecompressor.cs
@@ -0,0 +1,232 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Reflection;
+using System.Drawing.Text;
+using System.Windows.Forms;
+using System.IO.Compression;
+using System.Drawing.Drawing2D;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using Registry = Microsoft.Win32.Registry;
+using RegistryKey = Microsoft.Win32.RegistryKey;
+using PaintDotNet;
+using PaintDotNet.AppModel;
+using PaintDotNet.Clipboard;
+using PaintDotNet.IndirectUI;
+using PaintDotNet.Collections;
+using PaintDotNet.PropertySystem;
+using PaintDotNet.Rendering;
+using PaintDotNet.Effects;
+using ColorWheelControl = PaintDotNet.ColorBgra;
+using AngleControl = System.Double;
+using PanSliderControl = PaintDotNet.Pair<double,double>;
+using FilenameControl = System.String;
+using ReseedButtonControl = System.Byte;
+using RollControl = System.Tuple<double, double, double>;
+using IntSliderControl = System.Int32;
+using CheckboxControl = System.Boolean;
+using TextboxControl = System.String;
+using DoubleSliderControl = System.Double;
+using ListBoxControl = System.Byte;
+using RadioButtonControl = System.Byte;
+using MultiLineTextboxControl = System.String;
+
+[assembly: AssemblyTitle("Quadtree compressor plugin for Paint.NET")]
+[assembly: AssemblyDescription("Quadtree compressor selected pixels")]
+[assembly: AssemblyConfiguration("quadtree compressor")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Quadtree compressor")]
+[assembly: AssemblyCopyright("Copyright ©2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyMetadata("BuiltByCodeLab", "Version=6.4.7995.19949")]
+[assembly: SupportedOSPlatform("Windows")]
+
+namespace QuadtreecompressorEffect
+{
+    public class PluginSupportInfo : IPluginSupportInfo
+    {
+        public string Author
+        {
+            get
+            {
+                return base.GetType().Assembly.GetCustomAttribute<AssemblyCopyrightAttribute>().Copyright;
+            }
+        }
+
+        public string Copyright
+        {
+            get
+            {
+                return base.GetType().Assembly.GetCustomAttribute<AssemblyDescriptionAttribute>().Description;
+            }
+        }
+
+        public string DisplayName
+        {
+            get
+            {
+                return base.GetType().Assembly.GetCustomAttribute<AssemblyProductAttribute>().Product;
+            }
+        }
+
+        public Version Version
+        {
+            get
+            {
+                return base.GetType().Assembly.GetName().Version;
+            }
+        }
+
+        public Uri WebsiteUri
+        {
+            get
+            {
+                return new Uri("https://www.getpaint.net/redirect/plugins.html");
+            }
+        }
+    }
+
+    [PluginSupportInfo(typeof(PluginSupportInfo), DisplayName = "Quadtree compressor")]
+    public class QuadtreecompressorEffectPlugin : PropertyBasedEffect
+    {
+        public static string StaticName
+        {
+            get
+            {
+                return "Quadtree compressor";
+            }
+        }
+
+        public static Image StaticIcon
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public static string SubmenuName
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public QuadtreecompressorEffectPlugin()
+            : base(StaticName, StaticIcon, SubmenuName, new EffectOptions() { Flags = EffectFlags.Configurable })
+        {
+        }
+
+        public enum PropertyNames
+        {
+            Amount1
+        }
+
+        protected override PropertyCollection OnCreatePropertyCollection()
+        {
+            List<Property> props = new List<Property>();
+
+            props.Add(new Int32Property(PropertyNames.Amount1, 0, 0, 200));
+
+            return new PropertyCollection(props);
+        }
+
+        protected override ControlInfo OnCreateConfigUI(PropertyCollection props)
+        {
+            ControlInfo configUI = CreateDefaultConfigUI(props);
+
+            configUI.SetPropertyControlValue(PropertyNames.Amount1, ControlInfoPropertyNames.DisplayName, "Slider 1 Description");
+
+            return configUI;
+        }
+
+        protected override void OnCustomizeConfigUIWindowProperties(PropertyCollection props)
+        {
+            // Add help button to effect UI
+            props[ControlInfoPropertyNames.WindowHelpContentType].Value = WindowHelpContentType.PlainText;
+            props[ControlInfoPropertyNames.WindowHelpContent].Value = "Quadtree compressor v1.0\nCopyright ©2021 by \nAll rights reserved.";
+            base.OnCustomizeConfigUIWindowProperties(props);
+        }
+
+        protected override void OnSetRenderInfo(PropertyBasedEffectConfigToken token, RenderArgs dstArgs, RenderArgs srcArgs)
+        {
+            Amount1 = token.GetProperty<Int32Property>(PropertyNames.Amount1).Value;
+            base.OnSetRenderInfo(token, dstArgs, srcArgs);
+        }
+
+        protected override unsafe void OnRender(Rectangle[] rois, int startIndex, int length)
+        {
+            if (length == 0) return;
+            for (int i = startIndex; i < startIndex + length; ++i)
+            {
+                Render(DstArgs.Surface,SrcArgs.Surface,rois[i]);
+            }
+        }
+
+        #region User Entered Code
+        // Name: Quadtree compressor
+        // Submenu:
+        // Author:
+        // Title:
+        // Version:
+        // Desc:
+        // Keywords:
+        // URL:
+        // Help:
+        #region UICode
+        IntSliderControl Amount1 = 0;
+        #endregion
+        void Render(Surface dst, Surface src, Rectangle rect)
+        {
+            // Delete any of these lines you don't need
+            Rectangle selection = EnvironmentParameters.SelectionBounds;
+            //int centerX = ((selection.Right - selection.Left) / 2) + selection.Left;
+            //int centerY = ((selection.Bottom - selection.Top) / 2) + selection.Top;
+            /*
+            ColorBgra primaryColor = EnvironmentParameters.PrimaryColor;
+            ColorBgra secondaryColor = EnvironmentParameters.SecondaryColor;
+            int brushWidth = (int)EnvironmentParameters.BrushWidth;
+            */
+
+            QuadTree quadTree = new QuadTree(null, src, (0, 0), selection.Width, Amount1);
+            quadTree.compress();
+            quadTree.render(dst);
+
+            /*
+            ColorBgra currentPixel;
+            for (int y = rect.Top; y < rect.Bottom; y++)
+            {
+                if (IsCancelRequested) return;
+                for (int x = rect.Left; x < rect.Right; x++)
+                {
+                    currentPixel = src[x,y];
+                    // TODO: Add pixel processing code here
+                    // Access RGBA values this way, for example:
+                    // currentPixel.R = primaryColor.R;
+                    // currentPixel.G = primaryColor.G;
+                    // currentPixel.B = primaryColor.B;
+                    // currentPixel.A = primaryColor.A;
+                    if(centerX == x && centerY == y)
+                    {
+                        currentPixel = ColorBgra.Yellow;
+                    }
+                    dst[x,y] = currentPixel;
+                }
+            }
+            */
+        }
+        
+        #endregion
+    }
+}
